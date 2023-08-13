86742f156638168264819d5232d322192814c303
gruntcole <gruntcole1998@gmail.com>
2023-08-13 13:58:41 -0400

    cleaned up some comments

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,6 @@
 const client = require('./client');
 const { rebuildDB } = require('./seedData');
 
-// rebuildDB()
-//   .catch(console.error)
-//   .finally(() => client.end());
 client
 .connect()
 .then(rebuildDB)
