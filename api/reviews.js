2172c5765d6390afaaa0bbf5c86eb569f0325d8d
KierranyC <kierranyc@outlook.com>
2023-08-17 19:41:44 -0500

    seeing if we can see the site

--- api/reviews.js
+++ api/reviews.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const {
-    createReview,
-    getReviewByUserId,
-    getReviewByProductId
+  createReview,
+  getReviewByUserId,
+  getReviewByProductId
 } = require("../db/models");
 const router = express.Router();
 
@@ -14,31 +14,33 @@ router.get("/reviews", async (req, res, next) => {
 });
 
 router.post("/reviews", async (req, res, next) => {
-    const {userId, productId, body} = req.body;
-    try {
-        const review = createReview({userId, productId, body});
-        res.send(review);
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  router.get("/reviews/userId", async (req, res, next) => {
-    const {userId}= req.body;
-    try {
-        const reviews = getReviewByUserId(userId);
-        res.send(reviews);
-    } catch (error) {
-      next(error);
-    }
-  });
+  const { userId, productId, body } = req.body;
+  try {
+    const review = createReview({ userId, productId, body });
+    res.send(review);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  router.get("/reviews/:productId", async (req, res, next) => {
-    const {productId} = req.body;
-    try {
-        const reviews = getReviewByProductId(productId);
-        res.send(reviews);
-    } catch (error) {
-      next(error);
-    }
-  });
+router.get("/reviews/userId", async (req, res, next) => {
+  const { userId } = req.body;
+  try {
+    const reviews = getReviewByUserId(userId);
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/reviews/:productId", async (req, res, next) => {
+  const { productId } = req.body;
+  try {
+    const reviews = getReviewByProductId(productId);
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
